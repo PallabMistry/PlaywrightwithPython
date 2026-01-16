b58bdde832fd0857b3786ac3f89595efe0cf81b1
Pallab Mistry <pallabmistry90@gmail.com>
2026-01-16 23:50:17 +0600

    Add new commit

---
+++ tests/screenS.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+with sync_playwright() as p:
+    # Launch browser
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Open website
+    page.goto("https://example.com")
+
+    # Take screenshot
+    page.screenshot(path="example_screenshot.png", full_page=True)
+
+    # Close browser
+    browser.close()
