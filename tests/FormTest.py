bb131378ad9db1db253983367377d1044873d57a
Pallab Mistry <pallabmistry90@gmail.com>
2026-02-24 00:02:45 +0600

    form page

---
+++ tests/FormTest.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False,slow_mo=500)
+    page = browser.new_page()
+    page.goto("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php")
+    browser.close()
+
+
