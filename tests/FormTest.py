7f32fcfb6d730c131654d297e4e7e8ef41522a24
Pallab Mistry <pallabmistry90@gmail.com>
2026-02-26 00:25:33 +0600

    add new

--- tests/FormTest.py
+++ tests/FormTest.py
@@ -4,6 +4,12 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False,slow_mo=500)
     page = browser.new_page()
     page.goto("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php")
+    page.get_by_label('name').fill('Pallab')
+    page.get_by_label('email').fill('Pallab@gmail.com')
+    page.get_by_label('gender').click()
+    page.get_by_label('mobile').fill('980756432')
+    
+
     browser.close()
 
 
